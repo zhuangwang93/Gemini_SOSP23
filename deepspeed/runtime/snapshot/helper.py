021ce8fec4377d957e8012ead0bfdbcd02214430
Zhuang Wang <tmwangzhuanoutlook.com>
2023-07-21 17:06:21 +0000

    update github path

--- deepspeed/runtime/snapshot/helper.py
+++ deepspeed/runtime/snapshot/helper.py
@@ -2,6 +2,7 @@ import json
 from threading import Thread
 import socket
 import sys
+import os
 import subprocess
 
 
@@ -44,17 +45,20 @@ class etcdKey():
 
 def sync_code(ips_list):
     for ip in ips_list:
-        username = "zhuangwang93"
-        passcode = "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"
-        failure_recovery_pull = f"git pull https://{username}:{passcode}@github.com/zhuangwang93/failure_recovery.git"
-        example_pull = f"git pull https://{username}:{passcode}@github.com/zhuangwang93/DeepSpeedExamples.git"
-        sync_code_command = f"""
-            cd ~/zhuang/failure_recovery; {failure_recovery_pull}; cd ~/zhuang/DeepSpeedExamples; git stash; {example_pull}
-        """
+        gemini_dir = "~/zhuang/Gemini"
+        gemini_github = "https://github.com/zhuangwang93/Gemini.git"
+        if not os.path.exists(gemini_dir):
+            os.makedirs(gemini_dir)
+            sync_code_command = f"""
+                cd {gemini_dir}; git clone {gemini_github}
+            """
+        else:
+            sync_code_command = f"""
+                cd {gemini_dir}; git pull {gemini_github}
+            """
         print(f"********{ip} sync code********")
         run_sync_command = f"ssh -T {ip} '{sync_code_command}'"
         subprocess.call(run_sync_command, shell=True)
-
 
 
 def setup_instances(ips_list):
