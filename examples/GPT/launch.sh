8eab2336d3bf2ec7f00c7eff2b0c2ffcee63076d
Zhuang Wang <tmwangzhuanoutlook.com>
2023-07-23 21:29:42 +0000

    update 5B batch size

--- examples/GPT/launch.sh
+++ examples/GPT/launch.sh
@@ -3,7 +3,7 @@
 HOSTFILE="../hostfile"
 TRAIN_SCRIPT=pretrain_gpt.py
 TRAIN_CONFIG="
-10B_template_2.json
+5B_template.json
 "
 
 JOB_NAME=GPT2
@@ -21,21 +21,21 @@ ${deepspeed} --hostfile=${HOSTFILE} ${TRAIN_SCRIPT} \
 --deepspeed \
 --deepspeed_config $TRAIN_CONFIG \
 --job_name ${JOB_NAME} \
---max_steps 20 \
+--max_steps 24 \
 --print_steps 1 \
 --output . \
---comm_profile_steps 12 \
+--comm_profile_steps 16 \
 --jump_profile_lines 8 \
 --enable_comm_profile \
---snapshot_mode none \
---network_bandwidth 60 \
+--snapshot_mode naive \
+--network_bandwidth 80 \
 --snapshot_buffer_size 16 \
 --span_threshold 100 \
 --span_alpha 0.5 \
---max_blocks_in_span 6 \
+--max_blocks_in_span 4 \
 # --enable_snapshot_profile \
 # --save_to_disk \
 "
 
 echo $ds_cmd
-eval $ds_cmd | tee log
+eval $ds_cmd | tee log_5B_naive
