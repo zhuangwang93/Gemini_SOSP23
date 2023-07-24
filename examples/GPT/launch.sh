fa01dcf96ee1dc1829776b0301e2784079945d96
Zhuang Wang <tmwangzhuanoutlook.com>
2023-07-24 05:08:11 +0000

    5B configuration

--- examples/GPT/launch.sh
+++ examples/GPT/launch.sh
@@ -3,7 +3,7 @@
 HOSTFILE="../hostfile"
 TRAIN_SCRIPT=pretrain_gpt.py
 TRAIN_CONFIG="
-3B_template.json
+5B_template.json
 "
 
 JOB_NAME=GPT2
@@ -21,21 +21,21 @@ ${deepspeed} --hostfile=${HOSTFILE} ${TRAIN_SCRIPT} \
 --deepspeed \
 --deepspeed_config $TRAIN_CONFIG \
 --job_name ${JOB_NAME} \
---max_steps 24 \
+--max_steps 20 \
 --print_steps 1 \
 --output . \
---comm_profile_steps 16 \
+--comm_profile_steps 12 \
 --jump_profile_lines 8 \
 --enable_comm_profile \
---snapshot_mode none \
+--snapshot_mode interleave \
 --network_bandwidth 80 \
---snapshot_buffer_size 16 \
+--snapshot_buffer_size 32 \
 --span_threshold 100 \
---span_alpha 0.5 \
---max_blocks_in_span 4 \
+--span_alpha 0.8 \
+--max_blocks_in_span 8 \
 # --enable_snapshot_profile \
 # --save_to_disk \
 "
 
 echo $ds_cmd
-eval $ds_cmd | tee log_5B_naive
+eval $ds_cmd | tee log_5B_interleave
