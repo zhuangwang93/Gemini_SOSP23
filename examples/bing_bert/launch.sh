b312c08da8393e2d077e3132b11d1cd80646c42c
Zhuang Wang <tmwangzhuanoutlook.com>
2023-07-25 21:18:47 +0000

    test bert

--- examples/bing_bert/launch.sh
+++ examples/bing_bert/launch.sh
@@ -1,36 +1,30 @@
-export NCCL_SOCKET_IFNAME=eth0
+#!/bin/bash
+
+HOSTFILE="../hostfile"
+TRAIN_SCRIPT=deepspeed_train.py
+TRAIN_CONFIG="bert_large.json"
 
 deepspeed="/home/ec2-user/.local/bin/deepspeed"
 
-${deepspeed} --hostfile=hostfile \
-    deepspeed_train.py \
+${deepspeed} --hostfile=${HOSTFILE} \
+    ${TRAIN_SCRIPT} \
     --deepspeed \
     --deepspeed_config  deepspeed_adam_config.json \
-    --cf bert_base.json \
+    --cf ${TRAIN_CONFIG} \
     --max_seq_length 512 \
-    --max_steps 48 \
+    --max_steps 24 \
     --print_steps 1 \
     --use_nvidia_dataset \
     --deepspeed_transformer_kernel \
     --output . \
-    --snapshot_mode interleave \
-    --comm_profile_steps 32 \
+    --comm_profile_steps 16 \
+    --jump_profile_lines 12 \
     --enable_comm_profile \
-    # --enable_snapshot_profile \
-
-
-# ${deepspeed} --num_nodes 1 \
-#     deepspeed_train.py \
-#     --deepspeed \
-#     --deepspeed_config  deepspeed_adam_config.json \
-#     --cf bert_base.json \
-#     --max_seq_length 512 \
-#     --max_steps 32 \
-#     --print_steps 1 \
-#     --use_nvidia_dataset \
-#     --deepspeed_transformer_kernel \
-#     --output . \
-#     --snapshot_mode interleave \
-#     --comm_profile_steps 20 \
-    # --enable_comm_profile \
+    --snapshot_mode interleave \
+    --network_bandwidth 80 \
+    --snapshot_buffer_size 32 \
+    --span_threshold 100 \
+    --span_alpha 0.8 \
+    --max_blocks_in_span 16 \
+    # --save_to_disk \
     # --enable_snapshot_profile \
